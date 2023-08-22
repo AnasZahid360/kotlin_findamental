fca4814a8c5be0c9680b95fc3ed1ea42fdfc2eac
AnasZahid360 <anaszahid572@gmail.com>
2023-08-22 16:43:34 +0500

    21-Aug-2023 22-Aug-2023

--- app/src/main/java/eu/tutorials/kotlin_findamental/MainActivity.kt
+++ app/src/main/java/eu/tutorials/kotlin_findamental/MainActivity.kt
@@ -3,16 +3,34 @@ package eu.tutorials.kotlin_findamental
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnClickMe = findViewById<Button>(R.id.mybutton)
-        btnClickMe.setOnClickListener {
-            btnClickMe.text = "HAHA YOU CLICKED ME"
+        val btnIncrement = findViewById<Button>(R.id.mybutton)
+        val tvMyTextView = findViewById<TextView>(R.id.textView)
+        val btnReset = findViewById<Button>(R.id.mybutton2)
+        val btnDecrement = findViewById<Button>(R.id.mybutton3)
+        var timesClicked = 0
+        btnIncrement.setOnClickListener {
+            timesClicked ++
+
+            tvMyTextView.text = timesClicked.toString()
+        }
+
+        btnReset.setOnClickListener {
+            timesClicked = 0
+            tvMyTextView.text = timesClicked.toString()
+        }
+btnDecrement.setOnClickListener {
+    timesClicked --
+
+    tvMyTextView.text = timesClicked.toString()
+}
         }
 
     }
-}
+
