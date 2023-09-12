936f321c3e97c2cd0b4e290934fb2e80bf71b9f3
AnasZahid360 <anaszahid572@gmail.com>
2023-09-12 15:21:59 +0500

    12-Sept-23

--- app/src/main/java/eu/tutorials/kotlin_findamental/MainActivity.kt
+++ app/src/main/java/eu/tutorials/kotlin_findamental/MainActivity.kt
@@ -6,52 +6,15 @@ import android.widget.Button
 import android.widget.TextView
 
 fun main(){
-    //Arithmetic operators (+, -, *, /, %)
-    var result = 5+3
-    result = result / 2
-    // alternatively
-    // result /= 2
-    result = result * 5
-    result = result - 1
-    var moduloResult = 5%2
-    println( moduloResult)
-
-    //Comparison operators (==, !=, <, >, <=, >=)
-    val isEqual = 5==3
-    // Concatenation - adding of "Strings"
-    println("isEqual is " + isEqual)
-    val isNotEqual = 5!=5
-    // Kotlin has a feature called String Interpolation.
-    // This feature allows you to directly insert a template expression inside a String.
-    // Template expressions are tiny pieces of code that are evaluated and
-    // their results are concatenated with the original String.
-    // A template expression is prefixed with $ symbol.
-    // Following are examples of String interpolation
-    println("isNotEqual is $isNotEqual")
-
-    println("is5Greater3 ${5 > 3}")
-    println("is5LowerEqual3 ${5 >= 3}")
-    println("is5LowerEqual5 ${5 >= 5}")
-
-    //Assignment operators (+=, -=, *=, /=, %=)
-    var myNum = 5
-    myNum += 3
-    println("myNum is $myNum")
-    myNum *= 4
-    println("myNum is $myNum")
-
-
-    //Increment & Decrement operators (++, --)
-    myNum++
-    println("myNum is $myNum")
-    // increments after use
-    println("myNum is ${myNum++}")
-    // increments before use
-    println("myNum is ${++myNum}")
-    println("myNum is ${--myNum}")
-
-
-    var heightPerson1 = 170
-    var heightPerson2 = 189
-
+    val iphone = MobilePhone("iOS", "Apple", "iPhone 12")
+    val galaxyS20 = MobilePhone("Android","Samsung", "Galaxy S20")
+    val mateXS = MobilePhone("Android", "Huawei", "Mate X S")
 }
+
+class MobilePhone(osName: String, brand: String, model: String){
+    init {
+        println("The phone $model from $brand uses $osName as its Operating System")
+    }
+}
+
+
